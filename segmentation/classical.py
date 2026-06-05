49ea21eb3adb9320bff53f10361b662ca72b153b
Danialmohebbi <daniyalmohebbi@yahoo.com>
2026-06-05 22:40:04 +0200

    Add ga and cma

--- segmentation/classical.py
+++ segmentation/classical.py
@@ -1,6 +1,8 @@
 import numpy as np
 from skimage.filters import threshold_multiotsu
 from sklearn.cluster import KMeans
+from fitness.kapur import kapur_entropy_fitness
+from mealpy import GA,ES
 
 def apply_thresholds(channel, thresholds):
     segmented_image = np.zeros_like(channel, dtype=np.float32)
@@ -47,3 +49,22 @@ def kmeans_thresholding(image, k, mode="color"):
             segmented_image[:, :, channel] = cluster_centers[y_pred].reshape(image[:, :, channel].shape)
             thresholds.extend(sorted(cluster_centers.tolist()))
         return segmented_image.astype(np.uint8), thresholds
+
+def ga(image, k, mode="color", epoch=100, pop_size=50, crossover_rate=0.8, mutation_rate=0.2):
+    problem = SegmentationProblem(image, k, mode)
+    model= GA.BaseGA(epoch=epoch, pop_size=pop_size, pc=crossover_rate, pm=mutation_rate)
+    model.solve(problem)
+    
+    thresholds = np.sort(model.g_best.solution).clip(1,254).astype(np.uint8).tolist()
+    
+    return thresholds, model.g_best.target.fitness, model.history.list_global_best_fit
+
+def cmaes(image, k, mode="color", epoch=100, pop_size=50):
+    problem = SegmentationProblem(image, k, mode)
+    model = ES.CMA_ES(epoch=epoch, pop_size=pop_size)
+    model.solve(problem)
+    
+    thresholds = np.sort(model.g_best.solution).clip(1,254).astype(np.uint8).tolist()
+    
+    return thresholds, model.g_best.target.fitness, model.history.list_global_best_fit
+
